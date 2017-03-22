f5c9a9996a2e0aec446b7bc7a28db2765599cf09
Jorge Saud <jorgelsaud@gmail.com>
2017-03-22 16:56:06 -0400

    fix equipo

--- vc-elements/Equipo.php
+++ vc-elements/Equipo.php
@@ -1,187 +1,151 @@
 <?php
-/*
-Element Description: VC Info Box
-*/
+vc_map( array(
+    "name" => __("Equipo de Trabajo"),
+    "base" => "equipo",
+    "content_element" => true,
+    "show_settings_on_create" => false,
+    "is_container" => true,
+    'weight'=>10,
+    'category'=>__('Structure'),
+    'group'=>__('Partes Baquedano'),
+    'icon'=>get_template_directory_uri().'/assets/img/logo_baquedano.png',
+    "params" => array(
+        // add params same as with any other content element
+        array(
+            "type" => "textfield",
+            'holder' => 'h1',
+            "heading" => __("Titulo"),
+            "value"=>"Nuestro Equipo",
+            "param_name" => "titulo",
+            'weight'=>10,
+            "description" => __("Titulo de Equipo.")
+            ),
+        array(
+            "type" => "textfield",
+            'holder' => 'h1',
+            "heading" => __("Sub Titulo"),
+            "value"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "param_name" => "sub_titulo",
+            'weight'=>10,
+            "description" => __("Sub Titulo de Equipo.")
+            )
+        ),
+    "as_parent" => array('only' => 'miembro'), // Use only|except attributes to limit child shortcodes (separate multiple values with comma)
 
-// Element Class 
-class vcEquipo extends WPBakeryShortCode {
+    "js_view" => 'VcColumnView'
+    ) );
+add_shortcode( 'equipo', 'vc_equipo_html'  );
+function vc_equipo_html( $atts,$content ) {
+    extract(shortcode_atts(array(
+        'title'=>'Nuestro Equipo',
+        'sub_title'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        ),$atts));
+    $miembro=array(
 
-    // Element Init
-    function __construct() {
-        add_action( 'init', array( $this, 'vc_equipo_mapping' ) );
-        add_shortcode( 'vc_equipo', array( $this, 'vc_equipo_html' ) );
-    }
-
-    // Element Mapping
-    public function vc_equipo_mapping() {
-
-        // Stop all if VC is not enabled
-        if ( !defined( 'WPB_VC_VERSION' ) ) {
-            return;
-        }
-            // Map the block with vc_map()
-        vc_map(
-            array(
-                'name'=>__('Equipo de Trabajo','baquedano'),
-                'base'=>'vc_equipo',
-                'description'=>__('Equipo de Trabajo estilo Nosotros'),
-                'class'=>'mainSlider',
-                'show_settings_on_create'=>true,
-                'weight'=>10,
-                'category'=>__('Structure'),
-                'group'=>__('Partes Baquedano'),
-                'icon'=>get_template_directory_uri().'/assets/img/logo_baquedano.png',
-                'params'=>array(
-
-                    array(
-                        'type' => 'textfield',
-                        'holder' => 'h1',
-                        'class' => 'section-header',
-                        'heading' => __( 'Title' ),
-                        'param_name' => 'title',
-                        'value' => __( 'Nuestro Equipo', 'baquedano' ),
-                        'description' => __( 'Titulo', 'baquedano' ),
-                        'admin_label' => false,
-                        'weight' => 0,
-                        'group' => 'Partes Baquedano',
-                        ),  
-                    array(
-                        'type' => 'textfield',
-                        'holder' => 'h2',
-                        'class' => 'section-header',
-                        'heading' => __( 'Sub Title' ),
-                        'param_name' => 'sub_title',
-                        'value' => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'baquedano' ),
-                        'description' => __( 'Sub Titulo', 'baquedano' ),
-                        'admin_label' => false,
-                        'weight' => 0,
-                        'group' => 'Partes Baquedano',
-                        ),  
-                    array(
-                        'type'=>'textfield',
-                        'heading' => __( 'Cantidad de Miembros A Mostrar' ),
-                        'param_name' => 'cantidad',
-                        'value' => 3,
-                        'description' => __( 'Cantidad de Miembros A Mostrar' ),
-                        'admin_label' => true,
-                        'weight' => 0,
-                        'group' => 'Partes Baquedano',
-
-                        ),
-                    )
-                )
-            );
-
-    } 
+        );
+    ob_start();  
+    ?>
 
 
-    // Element HTML
-    public function vc_equipo_html( $atts ) {
-        extract(shortcode_atts(array(
-            'slides'=>3,
-            'title'=>'Nuestro Equipo',
-            'sub_title'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-
-            ),$atts));
-
-        $args=array(
-            'post_type' => 'equipo',
-            'posts_per_page'=>$slides
-            );
-        $the_query = new \WP_Query( $args );
-        // The Loop
-        if ( $the_query->have_posts() ) {
-            $equipo=array();
-            while ( $the_query->have_posts() ) {
-                $the_query->the_post();
-                $excerpt = apply_filters( 'the_content', get_the_excerpt() );
-                $miembro=array(
-                    'nombre'=>get_the_title(),
-                    'cargo'=>get_field('cargo'),
-                    'imagen'=>get_the_post_thumbnail(null,'full'),
-                    'contenido'=>$excerpt,
-                    'link'=>get_permalink(),
-                    );
-                array_push($equipo,$miembro);
-            }
-            wp_reset_postdata();
-        } else {
-            $equipo=array(
-                array(
-                    'nombre'=>'Nombre y Apellido',
-                    'imagen'=>"<img src='' alt='imagen miembro'",
-                    'cargo'=>'cargo',
-                    'contenido'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare odio et massa dignissim.',
-                    'link'=>'#',
-                    ),
-                array(
-                    'nombre'=>'Nombre y Apellido',
-                    'imagen'=>"<img src='' alt='imagen miembro'",
-                    'cargo'=>'cargo',
-                    'contenido'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare odio et massa dignissim.',
-                    'link'=>'#',
-                    ),
-                array(
-                    'nombre'=>'Nombre y Apellido',
-                    'imagen'=>"<img src='' alt='imagen miembro'",
-                    'cargo'=>'cargo',
-                    'contenido'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare odio et massa dignissim.',
-                    'link'=>'#',
-                    )
-                );
-    // no posts found
-        }
-        ob_start();  
-        ?>
-
-
-        <div class="our-team-container">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 animate-effect">
-                        <h3><?= $title?> <i></i></h3>
-                        <h4><?= $sub_title?></h4>
-                        <div class="our-team">
-                            <ul class="team-slider clearfix">
-                                <?php 
-                                foreach($equipo as $miembro ){
-                                    ?>
-                                    <li class="zoom">
-                                        <div>
-                                            <figure class="img-box fade-effect">
-                                                <a href="<?=$miembro['link']?>"><?=$miembro['imagen']?></a>
-                                            </figure>
-                                            <div class="member-detail">
-                                                <span class="name"><a href="<?=$miembro['link']?>"><?=$miembro['nombre']?></a></span>
-                                                <span class="designation"><?=$miembro['cargo']?> <i></i></span>
-                                                <P>
-                                                    <?=$miembro['contenido']?>
-                                                </P>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <?php
-                                }
-                                ?>
-
-                            </ul>
-                        </div>
+    <div class="our-team-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 animate-effect">
+                    <h3><?= $title?> <i></i></h3>
+                    <h4><?= $sub_title?></h4>
+                    <div class="our-team">
+                        <ul class="team-slider clearfix">
+                            <?= do_shortcode($content)?>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <?php
-        $output_string = ob_get_contents();
-        ob_end_clean();
+    <?php
+    $output_string = ob_get_contents();
+    ob_end_clean();
 
-        return $output_string;
+    return $output_string;
 
         //.. the Code is in the next steps ..//
 
-    } 
+} 
+vc_map( array(
+    "name" => __("Miembro"),
+    "base" => "miembro",
+    "content_element" => true,
+    "as_child" => array('only' => 'equipo'), // Use only|except attributes to limit parent (separate multiple values with comma)
+    'weight'=>10,
+    'category'=>__('Structure'),
+    'group'=>__('Partes Baquedano'),
+    'icon'=>get_template_directory_uri().'/assets/img/logo_baquedano.png',
+    "params" => array(
+        // add params same as with any other content element
+        array(
+            "type" => "textfield",
+            'holder' => 'h2',
+            'value'=>'Nombre y Apellido',
+            "heading" => __("Nombre y Apellido"),
+            "param_name" => "nombre",
+            "description" => __("Nombre y Apellido.")
+            ),
+        array(
+            "type" => "textfield",
+            'holder' => 'h3',
+            'value'=>'cargo',
+            "heading" => __("cago"),
+            "param_name" => "cargo",
+            "description" => __("Cargo del Miembro.")
+            ),
+        array(
+            "type" => "attach_image",
+            "heading" => __("Imagen"),
+            "param_name" => "imagen",
+            'admin_label' => true,
+            "description" => __("Imagen Miembro.")
+            ),
+        array(
+            'type' => 'textarea_html',
+            'label' => __( 'Informacion', 'baquedano' ),
+            'holder' => 'p',
+            'param_name' => 'content',
+            'value'=>__('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec erat erat. Integer blandit, nulla quis fermentum hendrerit, nisi diam viverra metus, porta semper est ipsum et sapien. feugiat viverra diam.felis tortor, euismod ac tortor ut.</p>'),
+            'description' => __( 'Informacion', 'baquedano' ),
+            ),
+        array(
+            'type' => 'textfield',
+                    // 'holder'=>'div',
+            'heading' => __( 'link' ),
+            'param_name' => 'link',
+            'value' => __( '#', 'baquedano' ),
+            'admin_label' => true,
+            'weight' => 1,
+            )
+        )
+    ) );
+add_shortcode( 'miembro',  'vc_miembro'  );
+function vc_miembro( $atts,$content ) {
+    extract(shortcode_atts(array(
+        'nombre'=>'Nombre y Apellido',
+        'cargo'=>'cargo',
+        'imagen'=>36,
+        'link'=>'#'
+        ),$atts));
+    $contenido = $content;
+    $contenido = apply_filters( 'the_content', $contenido );
+    $contenido = str_replace( ']]>', ']]&gt;', $contenido );
+    $imagentag=wp_get_attachment_image($imagen,'full');
+    echo "<li class='zoom'><div><figure class='img-box fade-effect'><a href='$link'>$imagentag</a></figure><div class='member-detail'><span class='name'><a href='$link'>$nombre</a></span><span class='designation'>$cargo<i></i></span>$contenido</div></div></li>";
 
-} // End Element Class
-
-// Element Class Init
-new vcEquipo();    
+    return;
+} 
+if ( class_exists( 'WPBakeryShortCodesContainer' ) ) {
+    class WPBakeryShortCode_Equipo extends WPBakeryShortCodesContainer {
+    }
+}
+if ( class_exists( 'WPBakeryShortCode' ) ) {
+    class WPBakeryShortCode_Miembro extends WPBakeryShortCode {
+    }
+}
