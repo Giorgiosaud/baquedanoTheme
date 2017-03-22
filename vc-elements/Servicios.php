714a72a88e207ab0993eecf13e25b2a9b66eba16
Jorge Saud <jorgelsaud@gmail.com>
2017-03-22 16:23:12 -0400

    fix slider

--- vc-elements/Servicios.php
+++ vc-elements/Servicios.php
@@ -1,26 +1,26 @@
 <?php
 vc_map( array(
-	"name" => __("Servicios", "my-text-domain"),
+	"name" => __("Servicios"),
 	"base" => "servicios",
-    "content_element" => true,
-    "show_settings_on_create" => false,
-    "is_container" => true,
-    'weight'=>10,
-    'category'=>__('Structure'),
-    'group'=>__('Partes Baquedano'),
-    'icon'=>get_template_directory_uri().'/assets/img/logo_baquedano.png',
-    "params" => array(
+	"content_element" => true,
+	"show_settings_on_create" => false,
+	"is_container" => true,
+	'weight'=>10,
+	'category'=>__('Structure'),
+	'group'=>__('Partes Baquedano'),
+	'icon'=>get_template_directory_uri().'/assets/img/logo_baquedano.png',
+	"params" => array(
         // add params same as with any other content element
-    	array(
-    		"type" => "textfield",
-    		'holder' => 'h1',
-    		"heading" => __("Titulo"),
-    		"value"=>"Servicios",
-    		"param_name" => "titulo",
-    		'weight'=>1000,
-    		"description" => __("Titulo de Seccion.")
-    		)
-    	),
+		array(
+			"type" => "textfield",
+			'holder' => 'h1',
+			"heading" => __("Titulo"),
+			"value"=>"Servicios",
+			"param_name" => "titulo",
+			'weight'=>1000,
+			"description" => __("Titulo de Seccion.")
+			)
+		),
     "as_parent" => array('only' => 'servicio'), // Use only|except attributes to limit child shortcodes (separate multiple values with comma)
 
     "js_view" => 'VcColumnView'
@@ -30,14 +30,13 @@ function vc_service_html( $atts,$content ) {
 	extract(shortcode_atts(array(
 		'titulo'=>'Servicios'
 		),$atts));
-	$content_slides=str_replace("[servicios ",'[servicios estilo="Slides" ',$content);
-	$content_miniatura=str_replace("[servicios ",'[servicios estilo="Miniatura" ',$content);
+	$content_slides=str_replace("[servicio ",'[servicio estilo="Slides" ',$content);
+	$content_miniatura=str_replace("[servicio ",'[servicio estilo="Miniatura" ',$content);
 	ob_start();  
 	?>
 	<div class="service-container">
 		<div class="container">
 			<div class="row">
-
 				<div class="left-detail animate-effect">
 					<div id="slide-items" class="flexslider">
 						<ul class="slides">
@@ -48,11 +47,11 @@ function vc_service_html( $atts,$content ) {
 				<div class="right-detail animate-effect">
 					<h2><span><?= $titulo ?></span></h2>
 				</div>
-
 				<div class="col-xs-12">
 					<div id="carousel" class="flexslider">
 						<ul class="our-service slides animate-effect clearfix">
 							<?= do_shortcode($content_miniatura)?>
+
 						</ul>
 					</div>
 				</div>
@@ -60,6 +59,7 @@ function vc_service_html( $atts,$content ) {
 			</div>
 		</div>
 	</div>
+
 
 
 
@@ -88,24 +88,32 @@ vc_map( array(
     		"type" => "textfield",
     		'holder' => 'h2',
     		'value'=>'Servicio',
-    		"heading" => __("Titulo", "my-text-domain"),
+    		"heading" => __("Titulo"),
     		"param_name" => "titulo",
-    		"description" => __("Titulo Del Servicio.", "my-text-domain")
+    		"description" => __("Titulo Del Servicio.")
+    		),
+    	array(
+    		"type" => "textfield",
+    		'holder' => 'h2',
+    		'value'=>'Servicio',
+    		"heading" => __("Sub Titulo"),
+    		"param_name" => "sub_titulo",
+    		"description" => __("Sub Titulo Del Servicio.")
     		),
     	array(
     		'type' => 'textarea_html',
     		'label' => __( 'Informacion', 'baquedano' ),
     		'holder' => 'p',
     		'param_name' => 'content',
-    		'value'=>__('<h2>WWW.BAQUEDANOCONSULTORES.CL</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>','baquedano'),
+    		'value'=>__('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec erat erat. Integer blandit, nulla quis fermentum hendrerit, nisi diam viverra metus, porta semper est ipsum et sapien. feugiat viverra diam.felis tortor, euismod ac tortor ut.</p>'),
     		'description' => __( 'Informacion', 'baquedano' ),
     		),
     	array(
     		"type" => "attach_image",
-    		"heading" => __("Imagen", "my-text-domain"),
+    		"heading" => __("Imagen"),
     		"param_name" => "imagen",
     		'admin_label' => true,
-    		"description" => __("Imagen Slide.", "my-text-domain")
+    		"description" => __("Imagen Slide.")
     		),
 
     	array(
@@ -119,23 +127,24 @@ vc_map( array(
     		)
     	)
     ) );
-add_shortcode( 'slide_interno',  'vc_servicio_interno_html'  );
+add_shortcode( 'servicio',  'vc_servicio_interno_html'  );
 
 function vc_servicio_interno_html($atts,$content){
 	extract(shortcode_atts(array(
-		'titulo'=>__('WWW.BAQUEDANOCONSULTORES.CL. <i></i>'),
+		'titulo'=>__('Servicio 1'),
+		'sub_titulo'=>__('www.baquedanoconsultores.cl')
 		'imagen'=>'83',
 		'link'=>'#', 
 		'estilo'=>'Titulo'
 		),$atts));
 	if($estilo=="Slides"){
-		echo "<li><h2>$titulo<i></i></h2><strong>www.baquedanoconsultores.cl</strong>$excerpt<a href='$link' class='quote'>VER MÁS</a></li>";
+		echo "<li><h2>$titulo<i></i></h2><strong>$sub_titulo</strong> $content<a href='$link' class='quote'>VER MÁS</a></li>";
 	}
 	if($estilo=="Miniatura"){
 		$imagentag=wp_get_attachment_image($imagen,'Sliders Size');
-		echo "<li class='zoom'><a href='$link'><em class='fa fa-caret-up'></em><figure class='fade-effect'>$imagentag</figure> <strong> <i></i>$titulo</strong></a></li>";
+		echo "<li class='zoom'><a href='$link'> <em class='fa fa-caret-up'></em><figure class='fade-effect'>$imagentag</figure> <strong> <i></i>$titulo </strong> </a></li>";
 	}
-	
+
 }
 //Your "container" content element should extend WPBakeryShortCodesContainer class to inherit all required functionality
 if ( class_exists( 'WPBakeryShortCodesContainer' ) ) {
