a3b003311c65d3833bee99fb886a686ecd12234e
Jorge Saud <jorgelsaud@gmail.com>
2017-08-16 16:47:48 -0400

    test

--- vc-elements/Info.php
+++ vc-elements/Info.php
@@ -97,10 +97,10 @@ class vcInfo extends WPBakeryShortCode {
 
 
 	// Element HTML
-	public function vc_info_html( $atts ) {
+	public function vc_info_html( $atts,$content ) {
 		extract(shortcode_atts(array(
 			'title'=>__('¿Quienes Somos?'),
-			'content'=>__('Baquedano Consultores nace de la necesidad de un mejor financiamiento hipotecario, que es entregado haciendo competir a las distintas entidades (bancos, compañías de seguro, leasing), logrando mejores condiciones comerciales a nuestros clientes. Desde aquí se Proyecta al servicio de Corretaje de Propiedades, con la ventaja competitiva de conectar los clientes compradores con Créditos ya aprobados y los propietarios que requieren la venta, esto conjugado con el servicio de arriendo y administración de propiedades de uso habitacional. Ingresando finalmente al sector de terrenos Industriales y/o paños. Logrando la mejor oferta posible a los dueños de terreno o sitio.'),
+			// 'content'=>__('Baquedano Consultores nace de la necesidad de un mejor financiamiento hipotecario, que es entregado haciendo competir a las distintas entidades (bancos, compañías de seguro, leasing), logrando mejores condiciones comerciales a nuestros clientes. Desde aquí se Proyecta al servicio de Corretaje de Propiedades, con la ventaja competitiva de conectar los clientes compradores con Créditos ya aprobados y los propietarios que requieren la venta, esto conjugado con el servicio de arriendo y administración de propiedades de uso habitacional. Ingresando finalmente al sector de terrenos Industriales y/o paños. Logrando la mejor oferta posible a los dueños de terreno o sitio.'),
 			'imagenes'=>'50,51,52,53',
 			'primer_destacado'=>'<strong><span>PRIMER</span>DESTACADO</strong>',
 			'segundo_destacado'=>'<strong><span>SEGUNDO</span>DESTACADO</strong>',
